7b96a7f939f57be8f291f4adc47ee678b8246846
MoloZzz <mega_oleksii@ukr.net>
2024-02-13 14:39:31 +0200

    +web:

--- server/middleware/ErrorHandlingMiddleware.js
+++ server/middleware/ErrorHandlingMiddleware.js
@@ -7,6 +7,6 @@ module.exports = function (err, req, res , next){
         return res.status(err.status).json({message: err.message});
     }
 
-    return res.status(500).json({message:"Непередбачувана помилка!"});
+    return res.status(500).json({message:"Unexpected error!"});
     
 }
