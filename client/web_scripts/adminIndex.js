4c6b356039607b80b2faf0d503204b8fafe3ee05
MoloZzz <mega_oleksii@ukr.net>
2024-02-15 20:51:54 +0200

    +веб, адмін сторінка, можна додавати гуртожитки та студентів

--- client/web_scripts/adminIndex.js
+++ client/web_scripts/adminIndex.js
@@ -1,6 +1,6 @@
 async function addDormitory() {
-    const name = document.getElementById('name').value;
-    const dormNumber = document.getElementById('dormNumber').value;
+    const name = document.getElementById('dorm_name').value;
+    const dorm_number = document.getElementById('dormNumber').value;
     const address = document.getElementById('address').value;
 
     try {
@@ -9,17 +9,56 @@ async function addDormitory() {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ name, dorm_number: dormNumber, address }),
+            body: JSON.stringify({ name, dorm_number: dorm_number, address }),
         });
 
         if (response.ok) {
             alert('Гуртожиток успішно додано!');
-            // Можна додати додаткову логіку, наприклад, очистити форму або перенаправити користувача
+            document.getElementById('addDormitoryForm').reset();
         } else {
             const data = await response.json();
             alert(`Помилка: ${data.message}`);
         }
     } catch (error) {
         console.error('Помилка при відправці запиту:', error);
+    }
+}
+
+async function addStudent() {
+    try {
+        const email = document.getElementById('email').value;
+        const surname = document.getElementById('surname').value;
+        const name = document.getElementById('name').value;
+        const dormitory_num = document.getElementById('dormitory_num').value;
+        const roomId = document.getElementById('roomId').value;
+        const contact_info = document.getElementById('contact_info').value;
+
+        const response = await fetch('http://localhost:9999/api/student', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email,
+                surname,
+                name,
+                dormitory_num,
+                roomId,
+                contact_info,
+            }),
+        });
+
+        if (response.ok) {
+            alert('Студент успішно додано!');
+            // Очистка полів форми
+            document.getElementById('addStudentForm').reset();
+        } else {
+            const data = await response.json();
+            alert(`Помилка: ${data.message}`);
+        }
+
+    } catch (error) {
+        console.error('Помилка при додаванні студента:', error);
+        alert('Помилка при додаванні студента:', error);
     }
 }
