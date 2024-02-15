ce3900f1e93495d345350f24657421d204ee2e19
MoloZzz <mega_oleksii@ukr.net>
2024-02-15 21:11:26 +0200

    +веб додавання кімнат

--- client/web_scripts/adminIndex.js
+++ client/web_scripts/adminIndex.js
@@ -62,3 +62,39 @@ async function addStudent() {
         alert('Помилка при додаванні студента:', error);
     }
 }
+
+async function addRoom() {
+    try {
+        const block_number = document.getElementById('block_number').value;
+        const capacity = document.getElementById('capacity').value;
+        const free_capacity = document.getElementById('free_capacity').value;
+        const room_name = document.getElementById('room_name').value;
+        const dormitoryId = document.getElementById('dormitoryId').value;
+
+        const response = await fetch('http://localhost:9999/api/room', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                block_number,
+                capacity,
+                free_capacity,
+                room_name,
+                dormitoryId,
+            }),
+        });
+
+        if (response.ok) {
+            alert('Кімнату успішно додано!');
+            document.getElementById('addRoomForm').reset();
+        } else {
+            const data = await response.json();
+            alert(`Помилка: ${data.message}`);
+        }
+
+    } catch (error) {
+        console.error('Помилка при додаванні кімнати:', error);
+        alert('Помилка при додаванні кімнати:', error);
+    }
+}
