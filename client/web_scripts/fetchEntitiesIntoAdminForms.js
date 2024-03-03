86c774fdc03501c7b47b3f5de9205c0e70524946
MoloZzz <mega_oleksii@ukr.net>
2024-03-03 21:08:56 +0200

    +селект кімнати при додааванні студента

--- client/web_scripts/fetchEntitiesIntoAdminForms.js
+++ client/web_scripts/fetchEntitiesIntoAdminForms.js
@@ -5,6 +5,7 @@ async function fetchStudents() {
         return students;
     } catch (error) {
         console.error('Помилка при отриманні списку студентів:', error);
+        alert('Помилка при отриманні списку cтудентів:', error);
     }
 }
 
@@ -20,10 +21,21 @@ async function fetchDormitories() {
     }
 }
 
-async function setDormitoriesIntoSelect(){
+async function fetchRooms(){
+    try {
+        const response = await fetch('http://localhost:9999/api/room');
+        const rooms = await response.json();
+        return rooms;
+    } catch (error) {
+        console.error('Помилка при отриманні списку кімнат:', error);
+        alert('Помилка при отриманні списку кімнат:', error);
+    }
+}
+
+async function setDormitoriesIntoSelect(identificator){
 
     const dormitories = await fetchDormitories();
-    let selectDormitoriesElement = document.getElementById('student_dorm_number');
+    let selectDormitoriesElement = document.getElementById(identificator);
 
     let currentOption = 0;
 
@@ -38,4 +50,76 @@ async function setDormitoriesIntoSelect(){
       });
 }
 
-setDormitoriesIntoSelect();
+
+async function setRoomByDormNumberIntoSelect(identificator,selected_dormitoryId){
+
+    const rooms = await fetchRooms();
+    const selectRoomsElement = document.getElementById(identificator);
+
+    while (selectRoomsElement.options.length > 0) {
+        selectRoomsElement.remove(0);
+    }
+
+
+    let currentOption = 0;
+
+    rooms.forEach(room => {
+        if(room.dormitoryId == selected_dormitoryId){
+        let newRoomOption = document.createElement("option");
+        newRoomOption.value = currentOption;
+        newRoomOption.textContent = `${room.room_name} `;
+        newRoomOption.value = room.room_name;
+        selectRoomsElement.appendChild(newRoomOption);
+        
+        currentOption++;
+        }
+      });
+
+    //   if(currentOption == 0){
+    //     let newRoomOption = document.createElement("option");
+    //   }
+}
+
+async function getDormIdbyNumb(dorm_number){
+    try {
+        const dormResp = await fetch('http://localhost:9999/api/dormitory/get-by-dorm-num', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                dorm_number
+            }),
+        });
+
+        if(!dormResp.ok){
+            dormitory_num.value = '';
+            alert("Виникла помилка з get-by-dorm-num");
+            throw new Error("помилка з get-by-dorm-num");
+        }
+
+        const dorm = await dormResp.json();
+        return dorm.id;
+
+
+    }catch(e){
+        console.error('Помилка при отриманні гуртожитка по номеру:', error);
+        alert('Помилка при отриманні гуртожитка по номеру:', error);
+    }
+}
+
+setDormitoriesIntoSelect('student_dorm_number');
+setDormitoriesIntoSelect('room_dorm_number');
+setDormitoriesIntoSelect('worker_dorm_number');
+
+async function setRoomNameSelect(){
+    try{
+        const addStudent_dormitory_numberElement = document.querySelector('#student_dorm_number');
+        const selectedOption = addStudent_dormitory_numberElement.value;
+        const addStudent_dormitory_id = await getDormIdbyNumb(selectedOption);
+        await setRoomByDormNumberIntoSelect('student_room_name',addStudent_dormitory_id);
+    }catch(e){
+    console.error(e.message);
+    }
+}
+
